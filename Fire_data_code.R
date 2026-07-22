9c8173cd83c2cd80c24bb1ddb7244296369005d2
Sophia Dodson <scdodson@ucdavis.edu>
2026-07-21 19:16:31 -0700

    Update Fire_data_code.R

--- Fire_data_code.R
+++ Fire_data_code.R
@@ -1,2 +1,6 @@
+#loading in data and ensuring date column is dates.
 wl1_data <- read.csv("WL1_cleaned.csv")
+wl1_data$Census_date <- as.Date(wl1_data$Census_date)
 
+#getting number of plants in each transect for each year
+year_transect_counts <- table(wl1_data$Census_date, wl1_data$Transect_plot)
