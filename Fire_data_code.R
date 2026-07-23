d33ff5e00aad10eedca1c37021a09705cfdd681c
Sophia Dodson <scdodson@ucdavis.edu>
2026-07-22 17:18:41 -0700

    Update Fire_data_code.R

--- Fire_data_code.R
+++ Fire_data_code.R
@@ -1,6 +1,20 @@
-#loading in data and ensuring date column is dates.
+library("dplyr")
+#loading in data and filtering for phenology values of not "NA"
 wl1_data <- read.csv("WL1_cleaned.csv")
-wl1_data$Census_date <- as.Date(wl1_data$Census_date)
+plants_measured <- wl1_data %>% filter(Phenology != "NA")
 
-#getting number of plants in each transect for each year
-year_transect_counts <- table(wl1_data$Census_date, wl1_data$Transect_plot)
+
+#getting number of plants censused for each year
+plants_measured %>% count(Census_year)
+
+#filter for reproductive F and P and count total
+FP_reproductive <- plants_measured %>% filter(Phenology %in% c("F","P"))
+
+#count of F and P plants each year
+FP_reproductive %>% count(Census_year)
+
+avg_flower <- FP_reproductive %>% group_by(Census_year) %>% 
+              summarise(mean_num_flwr = mean(Num_flowers))
+
+avg_fruit <- FP_reproductive %>% group_by(Census_year) %>%
+              summarise(mean_num_fruit = mean(Num_fruits))
